6d83668ed54b4add70e2bb2d3bd722cd03d0af04
Wilson Aballey <willywallet001@gmail.com>
2022-08-05 22:56:20 +0000

    Footer Component

---
+++ src/Components for All Pages/Footer.js
@@ -0,0 +1,23 @@
+
+import React,{Component} from "react";
+
+var footer = ()=>{
+        return(
+            <footer className="footer">
+            <div className="inner-footer">
+                <div className="options">
+                    <ul className="links">
+                        
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="about.txt">About</a></li>
+                        <li><a href="#">Terms & conditions apply</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="outer-footer">
+                Copyright &copy; 2022. All rights reserved                
+            </div>
+        </footer>
+        )
+    }
+export default footer;
