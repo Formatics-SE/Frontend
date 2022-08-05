81c1b48dc7cf5255c8352f300385a62c6ae2ec73
Andrews Yeboah <yeboahandrews408@gmail.com>
2022-08-05 23:43:03 +0000

    feature for assigning marks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react"
-import './App.css';
+import StudentsTable from "./Components/AssignMarksPage/StudentTable"
 
 export default function App() {
   return (
       <div>
-        <h1>Hello World</h1>
+        <StudentsTable />
       </div>  
   )
 }
