ca20f4e6af1c92c71e3024bc6b728c8631b8aece
Andrews Yeboah <yeboahandrews408@gmail.com>
2022-08-06 08:26:15 +0000

    moving components into Lecturer's folder

---
+++ src/Components/LecturerPage/dummyDB.js
@@ -0,0 +1,167 @@
+// array of dictionaries to hold students data for display
+
+let data = [
+ 
+    {
+        id: 1,
+        firstName: "Andrews",
+        lastName: "Yeboah",
+        index: "825",
+        mark: 50,
+        cwa: 60
+        
+    },
+    {
+        id: 2,
+        firstName: "Bright",
+        lastName: "Adu",
+        index: "826",
+        mark: 0,
+        cwa: 43
+    },
+    {
+        id: 3,
+        firstName: "Micheal",
+        lastName: "Yedu",
+        index: "827",
+        mark:80,
+        cwa: 82
+    },
+    {
+        id: 4,
+        firstName: "Andrews",
+        lastName: "Amponsa",
+        index: "828",
+        mark: 45,
+        cwa: 78
+    },
+    {
+        id: 5,
+        firstName: "Jerome",
+        lastName: "Akumasi",
+        index: "829",
+        mark: 24,
+        cwa: 63
+    },
+    {
+        id: 6,
+        firstName: "Wilson",
+        lastName: "Wenawome",
+        index: "830",
+        mark: 0,
+        cwa: 48
+    },
+    {
+        id: 7,
+        firstName: "Enock",
+        lastName: "Boadi",
+        index: "831",
+        mark: 0,
+        cwa: 100
+    },
+    {
+        id: 8,
+        firstName: "Sammy ",
+        lastName: "Nyame",
+        index: "831",
+        mark: 0,
+        cwa: 50
+    },
+    {
+        id: 9,
+        firstName: "Agyei ",
+        lastName: "Evan",
+        index: "879",
+        mark: 10,
+        cwa: 76
+    },
+    {
+        id: 10,
+        firstName: "Israel ",
+        lastName: "Apiedu",
+        index: "880",
+        mark: 0,
+        cwa: 45
+    },
+    {
+        id: 11,
+        firstName: "Ababio ",
+        lastName: "Andrew",
+        index: "881",
+        mark: 12,
+        cwa: 90
+    },
+    {
+        id: 12,
+        firstName: "Paul ",
+        lastName: "Treve",
+        index: "882",
+        mark: 4,
+        cwa: 88
+    },
+    {
+        id: 13,
+        firstName: "Gyau ",
+        lastName: "Baffour",
+        index: "883",
+        mark: 62,
+        cwa: 56
+    },
+    {
+        id: 14,
+        firstName: "Oduro ",
+        lastName: "Kwame",
+        index: "884",
+        mark: 23,
+        cwa: 39
+    },
+    {
+        id: 15,
+        firstName: "Sammy ",
+        lastName: "Kuffour",
+        index: "885",
+        mark: 10,
+        cwa: 78
+    },
+    {
+        id: 16,
+        firstName: "Sammy ",
+        lastName: "Nyame",
+        index: "885",
+        mark: 30,
+        cwa: 57
+    },
+    {
+        id: 17,
+        firstName: "Abrefa",
+        lastName: "Clifford",
+        index: "886",
+        mark: 0,
+        cwa: 89
+    },
+    {
+        id: 18,
+        firstName: "Kofi ",
+        lastName: "Badu",
+        index: "887",
+        mark: 3,
+        cwa: 65
+    },
+    {
+        id: 19,
+        firstName: "Hello",
+        lastName: "World",
+        index: "888",
+        mark: 20,
+        cwa: 82
+    },
+    {
+        id: 20,
+        firstName: "Alabi ",
+        lastName: "John",
+        index: "889",
+        mark: 20,
+        cwa: 69
+    }
+]
+export default data
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import  ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+ReactDOM.render(document.getElementById('root'),
+<App />
 );
 
 // If you want to start measuring performance in your app, pass a function
