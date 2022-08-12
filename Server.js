681d303ca89d7ce175bc0594c89ff0415f6d2f3a
jaakumasi <jeromeakumasi01@gmail.com>
2022-08-12 16:31:31 +0000

    added main server file and .env file

---
+++ Server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+// initialize app
+const app = express();
+
+//allow cross-origin sharing
+app.use(cors({origin: '*'}));
+
+// connect to local mongodb database
+mongoose.connect('mongodb://127.0.0.1:27017', 
+    () => console.log('connected to mongodb'));
+
+// importing routers
+const StudentLoginRouter = require('./StudentLoginRouter');
+const LecturerLoginRouter = require('./LecturerLoginRouter');
+const CourseDataRouter = require('./CourseDataRouter');
+
+// using routers
+app.use('/studentlogin', StudentLoginRouter);
+app.use('/lecturerlogin', LecturerLoginRouter);
+app.use('/coursedata', CourseDataRouter);
+
+// assigning port
+app.listen(process.env.PORT, () => console.log('running on port 2022'));
