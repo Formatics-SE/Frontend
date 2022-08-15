caf2070687480c038216532607bc2f02eecf6ca3
Samuel Nyame <samuelbaafi309@gmail.com>
2022-08-15 03:05:05 +0000

    JS click event continued

--- src/Temp/poll.js
+++ src/Temp/poll.js
@@ -4,3 +4,9 @@ document.getElementById("poll button").addEventListener("click", function(){
 document.querySelector(".done").addEventListener("click", function(){
     document.querySelector(".popup").style.visibility = "hidden";
 })
+document.getElementById("options").addEventListener("click", function(){
+    document.querySelector(".add_option").style.visibility = "visible";
+})
+document.querySelector(".option_2").addEventListener("click", function(){
+    document.querySelector(".add_option").style.visibility = "hidden";
+})
