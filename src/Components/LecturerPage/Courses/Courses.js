65cb5d3c16644dc458ef0e64464a8a3be15dcba6
jaakumasi <jeromeakumasi01@gmail.com>
2022-08-15 20:49:03 +0000

    modified login page

--- src/Components/LecturerPage/Courses/Courses.js
+++ src/Components/LecturerPage/Courses/Courses.js
@@ -1,41 +1,41 @@
 import React from 'react'
 
 export default function Courses() {
-    return (
-        <div>
-               <div class="course1">
-      <select>
-        <option>Groups</option>
-        <option>Take Attendace</option>
-        <option>create a poll</option>
-        <div class="allocate">
-          <select>
-               <option> Individuals</option>
-                <option>Groups</option>
+  return (
+    <div>
+      <div class="course1">
+        <select>
+          <option>Groups</option>
+          <option>Take Attendace</option>
+          <option>create a poll</option>
+          <div class="allocate">
+            <select>
+              <option> Individuals</option>
+              <option>Groups</option>
             </select>
-        </div>
-      </select>
-      
-       
-    
-  </div> 
-  <div class="course1">
-      <select>
-        <option>Groups</option>
-        <option>Take Attendace</option>
-        <option>create a poll</option>
-        <div class="allocate">
-          <select>
-               <option> Individuals</option>
-                <option>Groups</option>
+          </div>
+        </select>
+
+
+
+      </div>
+      <div class="course1">
+        <select>
+          <option>Groups</option>
+          <option>Take Attendace</option>
+          <option>create a poll</option>
+          <div class="allocate">
+            <select>
+              <option> Individuals</option>
+              <option>Groups</option>
             </select>
-        </div>
-      </select>
-      
-       
-    
-  </div> 
-        </div>
-        
-    )
+          </div>
+        </select>
+
+
+
+      </div>
+    </div>
+
+  )
 }
