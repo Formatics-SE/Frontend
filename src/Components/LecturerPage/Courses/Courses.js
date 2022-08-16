e2cd5fcd72eb07a5fba601538a196fc7ca0df43c
Phancy-Andrew <aaantwi2@st.knust.edu.gh>
2022-08-16 11:49:21 +0000

    modifying dropdown button for first course

--- src/Components/LecturerPage/Courses/Courses.js
+++ src/Components/LecturerPage/Courses/Courses.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-export default function Courses() {
-    return (
-        <div>
-       <h1></h1>     
-        </div>
-    )
+function Courses() {
+  return (
+    <Dropdown as={ButtonGroup}>
+      <Button variant="success">Course1</Button>
+
+      <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="">Groups</Dropdown.Item>
+        <Dropdown.Item href="">Take Attendance</Dropdown.Item>
+        <Dropdown.Item href="">Create a Poll</Dropdown.Item>
+        <Dropdown.Item href="">Allocate Marks</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 }
+
+export default  Courses;
