5a19c5a891c643f09426d677ff87c4052484765b
jaakumasi <jeromeakumasi01@gmail.com>
2022-08-26 07:43:39 +0000

    modified floating nav

---
+++ src/Components/LecturerPage/Polls/PollInstance.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import './poll_instance.css'
+
+export default function PollInstance({ title, totalVotesCast, options }) {
+
+    const [options_s, setOptions_s] = useState([]);
+
+    function percentage(votes) {
+        return ((votes / 74) * 100).toFixed(2);
+    }
+
+    useEffect(() => {
+        const testData = [
+            {
+                option: 'Naruto',
+                votes: 24
+            },
+            {
+                option: 'Demon Slayer',
+                votes: 32
+            },
+            {
+                option: 'Voltron',
+                votes: 18
+            }
+        ];
+
+        const totalVotesCast = 74
+        const ops = testData?.map((obj, index) => {
+            return (<>
+                <div className='option_and_percent_votes' key={index}>
+                    <div className='option'>
+                        {obj.option}
+                    </div>
+                    <div className='percent_votes'>
+                        {percentage(obj.votes)}%
+                    </div>
+                </div>
+                <div className='progressbar rounded-pill' >
+                    <div className='progressbar_fill rounded-pill' style={{width: `${percentage(obj.votes)}%`}}></div>
+                </div>
+            </>)
+        })
+
+        setOptions_s(ops);
+
+    }, [])
+    return (
+        <div className='poll_instance'>
+            <div className='title'>
+                {/* {title} */}
+                Title
+            </div>
+            <div className='options'>
+                {options_s}
+            </div>
+            <div className='totalVotesCast'>
+                Total votes: <span>{totalVotesCast}</span>
+            </div>
+        </div>
+    )
+}
+
+/**
+ <div className='votes'>
+</div>
+<div className='percent_fill'></div>
+ */
--- src/Components/LecturerPage/Polls/Polls.js
+++ src/Components/LecturerPage/Polls/Polls.js
@@ -1,9 +1,175 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import FormLabel from "react-bootstrap/FormLabel";
+import Form from "react-bootstrap/Form";
+import { v4 } from 'uuid'
+import './polls.css'
+
+import PollInstance from './PollInstance'
 
 export default function Polls() {
+
+    const [showModal, setShowModal] = useState(false)
+    const [polls, setPolls] = useState([])
+
+    const [optionsCount, setOptionsCount] = useState(0)
+
+    const [options, setOptions] = useState([])
+
+    // adds one option to the list of poll options => 10 options maximum
+    function addOption() {
+        if (optionsCount === 10) {
+            console.log('up to 10')
+            return;
+        }
+
+        let tempOps = options;
+
+        // initially add two options
+        if (optionsCount === 0) {
+            console.log('zero')
+
+            for (let i = 0; i < 2; i++) {
+                let keyVal = v4()
+                tempOps[i] = (
+                    <div className='option' key={keyVal}>
+                        <Form.Control className='option_input' />
+                        <div className={`remove_option_btn ${keyVal}`} onClick={(e) => removeOption(e)}>&times;</div>
+                    </div>
+                )
+            }
+            setOptionsCount(2);
+        }
+        else {
+            console.log('not zero')
+
+            let keyVal = v4()
+            tempOps.push(
+                <div className='option' key={keyVal}>
+                    <Form.Control className='option_input' />
+                    {/* give the remove option btn a className equivalent to it's container's (options) key value */}
+                    <div className={`remove_option_btn ${keyVal}`} onClick={(e) => removeOption(e)}>&times;</div>
+                </div>
+            )
+            setOptionsCount(prevCount => prevCount + 1);
+        }
+
+        setOptions(tempOps);
+
+    }
+    // end
+
+    //  removes an option from the list of poll options
+    function removeOption(e) {
+        console.clear()
+        if (options.length === 2) return;
+
+        setOptions(ops => ops.filter(op => {
+            if (e.target.className.split(' ')[1] !== op.key) {
+                console.log('no match');
+                return true;
+            }
+            else {
+                console.log('match');
+                return false;
+            }
+        }))
+        setOptionsCount(prevCount => prevCount - 1);
+    }
+    // end
+
+    // confirm poll creation
+    function handleConfirm() {
+        let title = document.querySelector('#title').value;
+        let optionInputs = Array.from(document.querySelectorAll('.option_input'))
+        // console.log(optionInputs.map(op => op.value))
+
+        let newPoll = {
+            title: title,
+            options: optionInputs.map(op => new Object({ option: op.value }))
+        }
+
+        // setPolls(newPoll);
+
+
+        console.log(newPoll)
+
+        // optionInputs.forEach(op => console.log(op.value))
+
+    }
+    // end
+
+    useEffect(() => {
+        console.log('polls: ', polls)
+    }, [polls])
+
+    useEffect(() => {
+        // const polls_session = JSON.parse(sessionStorage.getItem('polls')) // array of poll objects
+        // console.log('polls_session: ', polls_session)
+
+        // setPolls(polls_session?.map(obj => {
+        //     return (
+        //         <PollInstance
+        //             title={obj?.title}
+        //             totalVotesCast={obj?.totalVotesCast}
+        //             options={obj.options}
+        //         />
+        //     )
+        // }));
+
+    }, [])
+
     return (
-        <div>
-             
+        <div className='polls'>
+            <div className='create_polls_header'>
+                <div><Button className='create_poll_btn'
+                    onClick={() => { addOption(); setShowModal(true) }}>Create Poll</Button></div>
+                <div className='total_polls'>Total Polls: {polls?.length}</div>
+            </div>
+            <div className='created_polls_container'>
+                {polls}
+                {/* <PollInstance />
+                <PollInstance />
+                <PollInstance />
+                <PollInstance /> */}
+            </div>
+
+            <Modal onHide={() => setShowModal(false)}
+                show={showModal}
+                backdrop='static'
+                id='modal'
+            >
+                <Modal.Body>
+                    <div id='modal_header'>
+                        Poll
+                        <Button id='close_btn'
+                            onClick={() => { setOptions([]); setOptionsCount(0); setShowModal(false) }}
+                        >
+                            &times;
+                        </Button>
+                    </div>
+                    <div>
+                        <div className='field mb-2'>
+                            <FormLabel htmlFor='title'>Title</FormLabel>
+                            <Form.Control id='title' />
+                        </div>
+                        <div className='options_label_and_add_options_btn'>
+                            <div className='options_label'>Options</div>
+                            <div><Button className='add_options_btn' onClick={addOption}>+</Button></div>
+                        </div>
+                        <div className='options_container'>
+                            {options}
+                        </div>
+                        <div>
+                            <Button className='confirm_btn' onClick={handleConfirm}>Confirm</Button>
+                        </div>
+                    </div>
+
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
+
+
