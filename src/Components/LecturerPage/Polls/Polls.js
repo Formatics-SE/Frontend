39962c3646584136145124edd8fa4e29512c36af
Samuel Nyame <samuelbaafi309@gmail.com>
2022-08-26 00:59:25 +0000

    Adds a poll  option to the already existing

--- src/Components/LecturerPage/Polls/Polls.js
+++ src/Components/LecturerPage/Polls/Polls.js
@@ -7,5 +7,26 @@ import Form from "react-bootstrap/Form";
 import './polls.css'
 import PollInstance from './PollInstance'
 
+export default function Polls() {
 
+    const [showModal, setShowModal] = useState(false)
+    const [polls, setPolls] = useState([])
 
+    const [optionsCount, setOptionsCount] = useState(3)
+
+    const [options, setOptions] = useState([])
+
+    // adds one option to the list of poll options
+    function addOption() {
+        if (optionsCount === 11) return; // 10 options maximum
+
+        setOptions(prevOps => [...prevOps,
+        <div className='option'>
+            <Form.Control />
+            <div className={`remove_option_btn ${optionsCount}`}
+                onClick={(e) => removeOption(e)}>&times;</div>
+        </div>
+        ]);
+
+        setOptionsCount(prevCount => prevCount + 1);
+    }
