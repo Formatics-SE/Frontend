64c7d4585d665639801b14ccde2889d5de993653
Andrews-408 <yeboahandrews408@gmail.com>
2022-08-28 20:39:03 +0000

    algorithm for grouping by cwa

--- src/Components/LecturerPage/Groups/RandomGroup.js
+++ src/Components/LecturerPage/Groups/RandomGroup.js
@@ -1,35 +1,31 @@
 import React from "react"
 import data from "../dummyDB"
 import "./RandomGroup.css"
-import {Card, Button, Navbar} from "react-bootstrap"
+import Card from "react-bootstrap/Card"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 
-export default function RandomGroup(){
-     const [value, setValue] = React.useState({
-        division: 2,
-        shuffle: false,
-        random_group_display: <div>No group has been created yet</div>
-    })
+export default function RandomGroup(props){
+     const [showRandomGroups, setValue] = React.useState(true)
     
-    const student_list = data.map(student=>{
-        return(
-            <li key={student.id}>{`${student.firstName} ${student.lastName}`}</li>
-    )
-    })
+    // const student_list = data.map(student=>{
+    //     return(
+    //         <li key={student.id}>{`${student.firstName} ${student.lastName}`}</li>
+    // )
+    // })
 
 
 
    
-    function handleChange(event){
-        const {name, value}= event.target
-        setValue(prev=>{
-            return{
-                ...prev,
-                [name]:value 
-            }
-        })
-    }
+    // function handleChange(event){
+    //     const {name, value}= event.target
+    //     setValue(prev=>{
+    //         return{
+    //             ...prev,
+    //             [name]:value 
+    //         }
+    //     })
+    // }
     
 
     
@@ -39,71 +35,69 @@ export default function RandomGroup(){
             let temp = list[i]
             list[i] = list[randomNumber]
             list[randomNumber] = temp
-        }
-        
-        
-        
+        }   
     }
-    function handleGroupCreation(){
-            let classList = data.map(student=>{
-                return `${student.firstName} ${student.lastName}`
+    function createArraysForGroups(numberOfGroups){
+        let emptyList = []
+        let groupContainer = []
+        for(let i=0;i<numberOfGroups;i++){
+            emptyList.push(groupContainer)
+        }
+        return emptyList
+    }
+    
+   let classList = data.map(student=>{
+         return `${student.firstName} ${student.lastName}: ${student.cwa}`
             })
             ShuffleList(classList)
-            let studentsPerGroup = Number(value.division)
-            let randomGroups = []
-            for(let i=0; i<classList.length;i= i + studentsPerGroup){
-                randomGroups.push(classList.slice(i,i+studentsPerGroup))
+            let studentsPerGroup = Number(props.value_prop)
+            let division = Math.ceil(classList.length/studentsPerGroup)
+            let  classDivision= []
+            for(let i=0; i<classList.length;i= i + division){
+                classDivision.push(classList.slice(i,i+division))
             }
-            const random_group = randomGroups.map(item=>{
-               return(
-                <Card className="cards-container">
-                    <Card.Body className="cards-body">
-                        <Card.Title className="cards-title">GROUP {randomGroups.indexOf(item) + 1}</Card.Title>
+            let randomGroups = createArraysForGroups(division) 
+            let x = 0
+            for(let i=0;i<classDivision.length;i++){
+                for(let j=0;j<classDivision[i].length;j++){
+                    if(x===randomGroups.length){
+                        x = 0
+                    }
+                    else{
+                    randomGroups[x].push(classDivision[i].pop(j))
+                    x += 1
+                    }
+                }
+            }
+            
+
+            let random_group = randomGroups.map(item=>{
+             return(
+             <Card className="cards-container">
+                <Card.Body className="cards-body">
+                    <Card.Title className="cards-title">GROUP {classDivision.indexOf(item) + 1}</Card.Title>
                         <Card.Text>
-                            {item.map(sub=>{
-                              return(
-                               <li>{sub}</li>
-                            )
-                    })}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                             {item.map(sub=>{
+                         return(
+                             <li>{sub}</li>
+                          )
+                         })}
+                 </Card.Text>
+            </Card.Body>
+            </Card>
                )
             }) 
     
-            setValue(prev=>{
-                return{
-                    ...prev,
-                    shuffle: true,
-                    random_group_display: random_group
-                }
-            })
             
-    }
     
     
     return(
-    <section>
-        <Navbar variant="secondary">
-            <div className="container-outer">    
-                <div className="container-inner">
-                    <label htmlFor="division" className="label">GROUPS OF:</label>
-                    <input type="number" id="division" name="division" value={value.division} className="division-box" min={2} max={10} onChange={handleChange}/>
-                </div>
-                <div>
-                    <Button variant="light" className="submit-button1" onClick={handleGroupCreation}>Create</Button>
-                    <Button variant="light" type="submit" className="submit-button" placeholder="submit">Submit</Button>
-                </div>
-            </div>
-        </Navbar>
+    <section>    
+        <h2 className="random-groups-heading">Groupings</h2>
+        <div className="random-groups">
+            {random_group}
+        </div>
 
-       {value.shuffle? <div className="random-groups">
-            {value.random_group_display}
-        </div>: <div >
-                    <ol className="class-list">
-                        {student_list}
-                    </ol>
-                </div>}
     </section>
     )
 }
