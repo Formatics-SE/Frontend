03602f091f4db87cd97218ff370ee5373fea3b34
jaakumasi <jeromeakumasi01@gmail.com>
2022-08-28 13:06:38 +0000

    fixed react not in scope error

--- src/Components/StudentPage/Courses/Courses.js
+++ src/Components/StudentPage/Courses/Courses.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
