40123f54b478dba7116ffeb0fd241a50ac97edd4
Wilson Aballey <willywallet001@gmail.com>
2022-09-17 12:45:28 +0000

    Poll selection in Student field

---
+++ src/Components/StudentPage/Polls/PollInstancePending.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { FaTrash } from "react-icons/fa";
+import "./poll_instance_pending.css";
+
+export default function PollInstancePending({
+  id,
+  title,
+  totalVotesCast,
+  options,
+  deletePoll,
+}) {
+  const [showModal, setShowModal] = useState(false);
+
+  const [options_s, setOptions_s] = useState([]);
+
+  useEffect(() => {
+    // const totalVotesCast = 74
+    const ops = options?.map((obj, index) => {
+      return (
+        <>
+          <div className="pending_option" key={index}>
+            {obj.option}
+          </div>
+        </>
+      );
+    });
+
+    setOptions_s(ops);
+  }, []);
+  return (
+    <div className="poll_instance_pending">
+      <div className="title_and_delete_container">
+        <div className="title">{title}</div>
+        <div>
+          <FaTrash className="delete_icon" onClick={() => setShowModal(true)} />
+        </div>
+      </div>
+      <div className="options">{options_s}</div>
+      <div className="totalVotesCast">
+        Total votes: <span>{totalVotesCast}</span>
+      </div>
+
+      {/* modal to confirm Poll deletion */}
+      <Modal
+        onHide={() => setShowModal(false)}
+        show={showModal}
+        backdrop="static"
+        id="modal"
+      >
+        <Modal.Body>
+          <div id="modal_header">
+            <span style={{ color: "rgb(163, 23, 140)" }}>Confirm Delete</span>
+            <Button
+              id="close_btn"
+              onClick={() => {
+                setShowModal(false);
+              }}
+            >
+              &times;
+            </Button>
+          </div>
+          <div id="confirm_text">Do you want to delete this poll ?</div>
+        </Modal.Body>
+        <Modal.Footer id="modal_footer">
+          <Button id="confirm_btn_d" onClick={() => deletePoll(id)}>
+            Confirm
+          </Button>
+          <Button id="cancel_btn_d" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+/**
+ <div className='votes'>
+</div>
+<div className='percent_fill'></div>
+ */
--- src/Components/StudentPage/Polls/Polls.js
+++ src/Components/StudentPage/Polls/Polls.js
@@ -1,30 +1,63 @@
-import React, { useEffect } from 'react'
-import './polls.css'
+import React, { useState, suseEffect } from "react";
+import "./polls.css";
 
 export default function Polls() {
+  const [pendingPolls, setPendingPolls] = useState([]);
+  const [participatedPolls, setParticipatedPolls] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    const polls_session = JSON.parse(sessionStorage.getItem("polls"));
+    const indexNumber = JSON.parse(sessionStorage.getItem("indexNumber"));
 
-    }, [])
+    polls_session.map((pollObj) => {
+      let match = pollObj.participants.find((index) => index === indexNumber);
+      if (match) {
+        setParticipatedPolls((prev) => [
+          ...prev,
+          <PollInstance
+            title={pollObj?.title}
+            totalVotesCast={pollObj?.totalVotesCast}
+            options={pollObj.options}
+          />,
+        ]);
+      }
+      else {
+        setPendingPolls((prev) => [
+            ...prev,
+            <PollInstance
+              title={pollObj?.title}
+              totalVotesCast={pollObj?.totalVotesCast}
+              options={pollObj.options}
+            />,
+          ]);
+      }
+    });
 
-    return (
-        <div className='student_polls'>
-            <div className='pending_polls'>
-                <div className='pending_polls_title'>
-                    Pending Polls
-                </div>
-                <div className='pending_polls_body'>
-                    
-                </div>
-            </div>
-            <div className='participated_polls'>
-                <div className='participated_polls_title'>
-                    Participated Polls
-                </div>
-                <div className='participated_polls_body'>
-                    
-                </div>
-            </div>
+    // setPolls(polls_session?.map(obj => {
+    //     return (
+    //         <PollInstance
+    //             title={obj?.title}
+    //             totalVotesCast={obj?.totalVotesCast}
+    //             options={obj.options}
+    //         />
+    //     )
+    // }));
+  }, []);
+
+  return (
+    <div className="student_polls">
+      <div className="pending_polls">
+        <div className="pending_polls_title">Pending Polls</div>
+        <div className="pending_polls_body">
+            {pendingPolls}
         </div>
-    )
+      </div>
+      <div className="participated_polls">
+        <div className="participated_polls_title">Participated Polls</div>
+        <div className="participated_polls_body">
+            {participatedPolls}
+        </div>
+      </div>
+    </div>
+  );
 }
