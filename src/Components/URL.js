9b07c7ecac59bae8101d51dee1c75b7919a5d7d1
jaakumasi <jeromeakumasi01@gmail.com>
2022-09-26 16:25:33 +0000

    changed server url

--- src/Components/URL.js
+++ src/Components/URL.js
@@ -1,3 +1,3 @@
-export const URL = 'http://127.0.0.1:2022';
-// export const URL = 'https://boiling-caverns-45988.herokuapp.com';
+export const URL = 'https://shielded-savannah-38229.herokuapp.com';
+// export const URL = 'http://127.0.0.1:2022';
 // https://shielded-savannah-38229.herokuapp.com/
