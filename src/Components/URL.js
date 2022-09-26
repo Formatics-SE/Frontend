4691d70afc77e889a33f527d795301b9d2627e4c
jaakumasi <jeromeakumasi01@gmail.com>
2022-09-26 16:52:48 +0000

    changed server url

--- src/Components/URL.js
+++ src/Components/URL.js
@@ -1,3 +1,1 @@
-export const URL = 'https://shielded-savannah-38229.herokuapp.com';
-// export const URL = 'http://127.0.0.1:2022';
-// https://shielded-savannah-38229.herokuapp.com/
+export const URL = 'https://warm-river-74604.herokuapp.com';
